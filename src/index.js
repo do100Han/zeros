bab0631e4a569ccb1b88f3dee5d6c98ca59c43ab
Александр Руткевич <a.rutkevich@webcom.lan>
2018-09-25 13:49:11 +0300

    working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  index = 0;
+  while (number > 0) {
+    if (number > 1) {
+      number /= 5;
+      index += Math.floor(number);
+    } else {
+      break;
+    }
+  }
+  return index;
 }
